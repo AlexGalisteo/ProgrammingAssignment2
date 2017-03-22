80d50b3b58712cd7959b866a7f77f124434f8a01
Alex Galisteo <alexgalisteo@gmail.com>
2017-03-22 01:47:21 +0100

    Comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## How to test it:
+#> m <-matrix(c(3, 64, 42, 71), 2, 2)
+#> x <- makeCacheMatrix(m)
+#> cacheSolve(x)
 
-## Write a short comment describing this function
+## Check result:
+#> cacheSolve(x) %*% m 
 
+
+## Matrix Class with get/set properties cached the result
+## and avoid the cost of redundant caculations 
 makeCacheMatrix <- function(x = matrix()) {
+  #saves original matrix
   i <- NULL
+  
+  #get/set original matrix
   set <- function(x_) {
     x <<- x_
     i <<- NULL
   }
   get <- function() x
+  #get/set matrix' inverse cached result
   setInverse <- function(inverse) i <<- inverse
   getInverse <- function() i
+  
   list(set = set, get = get,
        setInverse = setInverse,
        getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Cache 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+  ## Return the matrix cached value of the the inverse of 'x'
   i <- x$getInverse()
   if(!is.null(i)) {
-    message("getting cached data")
+    message("getting cached inverse")
     return(i)
   }
+  #Cache is empty, lets calculate the inverse...
   data <- x$get()
   i <- solve(data, ...)
+  #Save the result in the cache
   x$setInverse(i)
   i
 }
